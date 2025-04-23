89f031d697d44db7cc525e2e97358c720424dc9d
Super User <root@localhost.localdomain>
2025-04-24 00:51:10 +0300

    added changes to use puppet

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -43,7 +43,41 @@ resource "aws_instance" "wordpress" {
   key_name                    = aws_key_pair.wp_key.key_name
   vpc_security_group_ids      = [aws_security_group.wp_sg.id]
   associate_public_ip_address = true
-  user_data                   = file("${path.module}/wordpress.sh")
+
+  provisioner "file" {
+    source      = "${path.module}/wordpress.pp"
+    destination = "/tmp/wordpress.pp"
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.ssh/id_ed25519")
+      host        = self.public_ip
+    }
+  }
+
+ provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt install -y software-properties-common",
+      "sudo add-apt-repository -y universe",
+      "sudo apt update",
+      "wget https://apt.puppet.com/puppet7-release-jammy.deb -O /tmp/puppet7.deb",
+      "sudo dpkg -i /tmp/puppet7.deb",
+      "sudo apt update",
+      "sudo apt install -y puppet-agent",
+      "sudo ln -s /opt/puppetlabs/bin/puppet /usr/bin/puppet",
+      "sudo mkdir -p /var/www/html",
+      "sudo puppet apply /tmp/wordpress.pp"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.ssh/id_ed25519")
+      host        = self.public_ip
+    }
+  }
 
   tags = {
     Name = "wordpress-instance"
